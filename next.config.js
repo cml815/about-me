f00daa7ed33a0032c55665ab230a818389059dbc
Caroline Leopold <caroline.leopold@gmail.com>
2021-12-02 12:40:10 -0500

    initial config mdx

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+// next.config.js
+const hasMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+});
+
+const nextConfig = {
+  /** Tell Next that it should render MDX files as page if they're in the page directory. */
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+};
+
+/** Wrap the config so it can add all of the necessary configuration to process MDX iles. */
+module.exports = hasMDX(nextConfig);
+

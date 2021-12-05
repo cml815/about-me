3b0b95ee07625a006b296fd3fd2179b885908627
Caroline Leopold <caroline.leopold@gmail.com>
2021-12-05 14:01:41 -0500

    start new pages

---
+++ pages/healthcare.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
+
+export default function Healthcare () {
+  return (
+    
+<h1>Healthcare Stories</h1>
+
+  )
+  
+}
---
+++ pages/technology.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
+
+export default function Technology () {
+  return (
+    
+<h1>Technology Stories</h1>
+
+  )
+  
+}

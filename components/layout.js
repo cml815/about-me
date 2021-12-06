3c1d1003aa7b7afe974da8e75d96f0c990fdc821
Caroline Leopold <caroline.leopold@gmail.com>
2021-12-06 12:34:28 -0500

    add head to layout

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+
 
 export default function Layout ({ children }) {
-  return <div className={styles.headline}>{children}</div>
+  return
+  <> 
+  <Head>
+    <link rel="icon" href="/favicon.ico" />
+    <meta
+      name="description"
+      content="Learn about writer and web designer Caroline Leopold. View portfolio."
+        />
+  </Head>
+  <div className={styles.headline}>{children}</div>
+  </>
 }
 

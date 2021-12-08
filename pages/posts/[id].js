560bad7ade71b7936ff73ae33b7a46118a089727
Caroline Leopold <caroline.leopold@gmail.com>
2021-12-08 12:48:37 -0500

    apply title style to posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Layout from '../../components/layout'
 import Date from '../../components/date'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id)
@@ -16,14 +17,17 @@ export default function Post({ postData }) {
   return (
     <Layout>
       <Head>
-        {postData.title}
+        <title>{postData.title}</title>
       </Head>
       <br />
-        {postData.id}
+      <article>
+        {/* {postData.id} */}
+      <h1 className={utilStyles.headingTitle}>{postData.title} </h1>
       <br />
       <Date dateString={postData.date} />
       <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }

7973f9ae4d955bfaec5b5efd14429c2b264e4956
Caroline Leopold <caroline.leopold@gmail.com>
2021-12-10 12:22:46 -0500

    rm layout home modules

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
-import styles from '../components/layout.module.css'
+import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
 
 // https://nextjs.org/learn/basics/assets-metadata-css/polishing-layout
